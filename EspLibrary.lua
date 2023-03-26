48cf9773b40f73c48f5c372d4189953f0809fe18
DeividComSono <116926351+DeividComSono@users.noreply.github.com>
2023-03-26 12:46:25 -0300

    Update EspLibrary.lua

--- EspLibrary.lua
+++ EspLibrary.lua
@@ -1,3 +1,4 @@
+local RunService = game:GetService("RunService")
 local EspLibrary = {}
 
 function EspLibrary.TextEsp(name, color, obj, center)
@@ -81,6 +82,60 @@ function EspLibrary.LineEsp(color, obj)
             c1 = nil
             LineEsp.Visible = false
             LineEsp:Remove()
+        end
+    end
+
+    return ret
+end
+
+
+function EspLibrary.PlayerTextEsp(color, who, center)
+    local TextEsp = Drawing.new("Text")
+    TextEsp.Visible = false
+    TextEsp.Center = center
+    TextEsp.Outline = true
+    TextEsp.Color = color
+    TextEsp.Size = 16
+
+    local c1
+
+    c1 = RunService.RenderStepped:Connect(function()
+        if who.Character ~= nil and who.Character:FindFirstChild("Humanoid") ~= nil and who.Character:FindFirstChild("HumanoidRootPart") ~= nil and who ~= game.Players.LocalPlayer and who.Character.Humanoid.Health > 0 then
+            local pos, onscreen = Camera:WorldToViewportPoint(who.Character.HumanoidRootPart.Position)
+
+            if onscreen then
+                TextEsp.Position = Vector2.new(pos.X, pos.Y)
+                TextEsp.Text = who.Name
+
+                if teamcheck and who.TeamColor == game.Players.LocalPlayer.TeamColor then
+                    TextEsp.Visible = false
+                else
+                    TextEsp.Visible = true
+                end
+            else
+                TextEsp.Visible = false
+            end        
+        else
+            TextEsp.Visible = false
+            TextEsp:Remove()
+            c1:Disconnect()
+        end
+    end)
+
+    local ret = {}
+
+    ret.update = function(newText)
+        if c1 ~= nil then
+            name = newText
+        end
+    end
+
+    ret.delete = function()
+        if c1 then
+            c1:Disconnect()
+            c1 = nil
+            TextEsp.Visible = false
+            TextEsp:Remove()
         end
     end
 
